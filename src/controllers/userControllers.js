89d3519c4204b0d96b821e7919cd1a6d2a3187f4
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-03-31 11:46:42 +1100

    errorHandler

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,9 +1,23 @@
 import { registerUserModel } from "../models/users/UserModel.js";
+import { encryptPassword } from "../utils/bcrypt.js";
 
 export const registerUserController = async (req, res, next) => {
     try {
-        const formObj = req.body;
+        const { fName, lName, email } = req.body;
+        let { password } = req.body
+        let { confirmPassword } = req.body
+        password = await encryptPassword(password)
+        confirmPassword = await encryptPassword(confirmPassword)
+
+        const formObj = {
+            fName,
+            lName,
+            email,
+            password,
+            confirmPassword
+        }
         const data = await registerUserModel(formObj);
+
         console.log(data, 333)
         if (data) {
             return res.status(200).json({
--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -4,9 +4,9 @@ const SALT_ROUNDS = 10;
 
 // encrypting the password 
 export const encryptPassword = async (text) => {
-    return await bcrypt.hash(text, SALT_ROUNDS)
+    return bcrypt.hash(text, SALT_ROUNDS)
 }
 // comparing the password
 export const comparePassword = async (plainText, encryptText) => {
-    return await bcrypt.compare(plainText, encryptText)
+    return bcrypt.compare(plainText, encryptText)
 }
