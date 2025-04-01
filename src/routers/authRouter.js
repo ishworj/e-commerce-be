3653b9f2f67e0cb84c68ee78a4f6de9497515479
anuska <anuskasth2022@gmail.com>
2025-04-01 14:06:17 +1100

    user CRUD

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { registerUserController, signInUserController } from "../controllers/userControllers.js";
+import { deleteUserController, registerUserController, signInUserController, updateUserController } from "../controllers/userControllers.js";
 
 const router = express.Router()
 
 // routers
 router.post("/register", registerUserController)
 router.post("/signin", signInUserController)
+
+
+
+// // get user detail
+// router.get("/", getUserDetail);
+
+
+
+//update user
+router.put("/:id", updateUserController)
+
+//delete user
+router.delete("/:id", deleteUserController)
 
 export default router
