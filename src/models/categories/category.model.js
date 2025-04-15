48f9ee98e1c18741c96f5576b10a664343584bda
Ishwor karki <ishworkarkij11@gmail.com>
2025-04-15 11:02:52 +1000

    get category

--- src/models/categories/category.model.js
+++ src/models/categories/category.model.js
@@ -2,15 +2,19 @@ import CategorySchema from "./category.schema.js";
 
 // create a category
 export const createCategoryModel = (obj) => {
-    return CategorySchema(obj).save();
+  return CategorySchema(obj).save();
 };
 
 // updating a category
 export const updatingCategoryModel = (filter, obj) => {
-    return CategorySchema.findByIdAndUpdate(filter, obj, { new: true });
+  return CategorySchema.findByIdAndUpdate(filter, obj, { new: true });
 };
 
 // deleting a category
 export const deleteCategoryModel = (_id) => {
-    return CategorySchema.findByIdAndDelete(_id, { new: true });
+  return CategorySchema.findByIdAndDelete(_id, { new: true });
+};
+// fetchs all categories
+export const getCategoriesDB = () => {
+  return CategorySchema.find({});
 };
